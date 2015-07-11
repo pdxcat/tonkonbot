690112789966b79e54ceb7a2a9dc7442ed1f9390
Spencer Krum <nibz@spencerkrum.com>
2015-07-10 19:39:07 -0700

    Init DB

--- tonkonbot.py
+++ tonkonbot.py
@@ -149,6 +149,30 @@ class LogBotFactory(protocol.ClientFactory):
         reactor.stop()
 
 
+def init_db_or_pass(conn):
+    """
+    Attempt to validate if the database is set up, else create one
+    """
+    c = conn.cursor()
+    #c.execute('''CREATE TABLE stocks
+    #                     (date text, trans text, symbol text, qty real, price real)''')
+
+    # Insert a row of data
+    #c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+    # Save (commit) the changes
+    try:
+        c.execute("SELECT * FROM braindumps")
+        return
+    except sqlite3.OperationalError:
+        c.execute('''CREATE TABLE braindumps
+                             (date text, topic text, dumper text)''')
+
+    conn.commit()
+    print c.execute("SELECT * FROM braindumps")
+
+
+
 if __name__ == '__main__':
     # initialize logging
     log.startLogging(sys.stdout)
@@ -163,6 +187,7 @@ if __name__ == '__main__':
     # setup database
 
     conn = sqlite3.connect(config['db'])
+    init_db_or_pass(conn)
 
 
 
