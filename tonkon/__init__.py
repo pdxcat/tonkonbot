a05e95bea313eca98b60e10c9910f20b713fdb84
Caleb Boylan <calebb@cat.pdx.edu>
2016-01-26 11:41:44 -0800

    update regex for bd list

--- tonkon/__init__.py
+++ tonkon/__init__.py
@@ -28,7 +28,7 @@ commands.append(source)
 
 # list 5 braindumps that have not already passsed
 def bdlist(bot, user, channel, msg):
-    if re.match("^(\+)?!?bd( list)?( -a)?$", msg):
+    if re.match("^(\+|!)bd( list)?( -a)?$", msg):
         r = requests.get("http://web.cecs.pdx.edu/~finnre/braindumps")
         count = 0
 
